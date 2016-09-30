58f61652fc62050899172913547ee48809bd0a9f
WizzWYZ <wizz0526@gmail.com>
2016-09-30 16:36:57 +0800

    synchronize order

--- main/Node/sync.js
+++ main/Node/sync.js
@@ -1,86 +1,125 @@
 /**
  * Created by wyz on 16/9/29.
  */
+const express = require('express');
+const qs = require('querystring');
+const app = express();
+
 var mysql = require('mysql');
 const datefomat = require('dateformat');
 const uuid = require('uuid');
-
+const async = require('async');
 var pool = mysql.createPool({
-    connectionLimit: 500,
-    host: 'rubicware.mysql.rds.aliyuncs.com',
-    user: 'oms',
-    password: 'rub_oms123',
-    database: 'db_tms'
-});
-var poolRT = mysql.createPool({
-    connectionLimit: 200,
+    connectionLimit: 20,
     host: 'rm-bp1l07gc716p220p0i.mysql.rds.aliyuncs.com',
     user: 'runteng',
     password: 'runteng_123',
     database: 'db_order'
 });
+const md5 = require('md5');
 
-const pagination = 100;
-const code = ["t_bto", "t_cnp", "t_cnp2", "t_ems", "t_gto", "t_sto", "t_ttk", "t_yto", "t_yunda", "t_zto"];
-var request = code.map((table) => {
-    return new Promise((resolve) => {
-            pool.getConnection((err, connection)=> {
-                if (err){
-                    console.log(err);
-                    return;
-                }
-                var sql = "select count(1) from "+table+" where sync = '4' and DATE_FORMAT(`MODIFY_DATE`,'%Y%m%d') > '20160401'  and DATE_FORMAT(`MODIFY_DATE`,'%Y%m%d') < '20160801'";
-                connection.query(sql, (err, count) => {
-                    if (err) {
-                        console.log(err);
-                        return;
-                    }
-                    var number = count[0]['count(1)'];
-                    if (number == 0) {
-                        connection.release();
-                        return;
-                    }
-                    var page = [];
-                    var pages = 0;
-                    while (pages < Math.ceil(number / pagination)) {
-                        page.push(pages);
-                        pages++;
-                    }
-                    var subRequest = page.map((i)=> {
-                        return new Promise((solve) => {
-                            var subSql = "select * from "+ table +" where sync = '4' and DATE_FORMAT(`MODIFY_DATE`,'%Y%m%d') > '20160401' " +
-                                " and DATE_FORMAT(`MODIFY_DATE`,'%Y%m%d') < '20160801' LIMIT ?,?";
-                            connection.query(subSql, [i * pagination, (i + 1) * pagination - 1], (err, rows)=> {
-                                if (err) {
-                                    console.log(err);
-                                    connection.release();
-                                    return;
-                                }
-                                rows.forEach((row)=> {
-                                    poolRT.getConnection((err, rtconnection)=> {
-                                        if (err) {
-                                            console.log(err);
-                                            return;
-                                        }
-                                        var subsubSql = "insert into t_order values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
-                                        rtconnection.query(subsubSql, [uuid.v1(), row.ORDER_ID, row.ORDER_CODE, row.CUST, row.EXPRESS_NO, row.NAME, row.ADDRESS, row.WEIGHT, row.AREA_CODE, row.MOBILE, row.CREATE_DATE, row.TEL, row.PROVINCE, row.CITY, row.DISTRICT, row.SYNC, row.MSG, row.ITEM_QTY, row.EXPRESS_CODE, row.MODIFY_DATE],
-                                            (err)=> {
-                                                if (err) {
-                                                    console.log(err);
-                                                    return;
-                                                }
-                                                solve();
-                                            })
-                                    })
-                                })
-                            })
-                        })
-                    });
-                    Promise.all(subRequest).then(() => {resolve();});
-                })
 
-            })
-        }
-    )
+process.on('uncaughtException', function (err) {
+    console.log('Caught exception: ' + err);
+    console.log(err.stack);
+});
+
+var funs = [];
+app.get('/ping.rt', (req, res) => {
+    res.send("SUCCESS");
 })
-Promise.all(request).then(() => {pool.end();poolRT.end();});
+//用户
+app.post('/sync.rt', (req, res) => {
+    var datass = [];
+    req.on('data', (data) => {
+        datass.push(data);
+    });
+    req.on('end', ()=> {
+        try {
+            var param = datass.join('');
+            try {
+                param = qs.parse(decodeURIComponent(param));
+            } catch (e) {
+                console.log(e.stack);
+                res.send(JSON.stringify({
+                    success: false,
+                    error: "格式错误",
+                    result: decodeURIComponent(param)
+                }))
+            }
+            if (param.method != "sync" || param.appkey != "SKU360") {
+                res.send('{"success":false,"error":"参数错误"}');
+                return;
+            }
+            if (md5("SKU360" + "uksevol1237890" + param.datetime).toUpperCase() != param.sign) {
+                res.send('{"success":false,"error":"密码校验错误"}');
+                return;
+            }
+
+            try {
+                var datas = JSON.parse(param.data);
+                saveDatas(datas, 0);
+            } catch (e) {
+                console.log(e);
+            }
+        } catch (e) {
+            console.log(e.stack);
+            res.send(JSON.stringify({
+                success: false,
+                error: "系统内部错误"
+            }))
+        }
+        ;
+    })
+});
+function saveDatas(datas, step) {
+    if (step < 10) {
+        datas.forEach((data) => {
+            var order = {
+                ID: uuid.v1(),
+                ORDER_ID: data.ORDER_ID,
+                CUST: data.CUST,
+                EXPRESS_NO: data.EXPRESS_NO,
+                WEIGHT: data.WEIGHT,
+                PROVINCE: data.PROVINCE,
+                CITY: data.CITY,
+                EXPRESS_CODE: data.EXPRESS_CODE,
+                MODIFY_DATE: data.MODIFY_DATE
+            };
+            funs.push((cb)=> {
+                save(cb, order)
+            });
+        })
+        async.parallel(funs, (err, results)=> {
+            var finalResults = [];
+            results.forEach((row) => {
+                if (row != null) {
+                    finalResults.push(row);
+                }
+            });
+            if (finalResults.length!=0) {
+                saveDatas(finalResults, step + 1);
+            }
+        });
+    }
+}
+
+function save(cb, order) {
+    pool.getConnection((err, connection) => {
+        if (err) {
+            console.log(err);
+            cb(null, order);
+        }
+        connection.query("INSERT INTO t_order SET ? ", order, (err, rows) => {
+            connection.release();
+            if (err) {
+                console.log(err);
+                cb(null, order);
+            }
+            cb(null);
+        });
+    });
+}
+
+
+app.listen(process.argv[2]);
